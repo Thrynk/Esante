5f78345fdce747eb75b767e9243c573c3ccf9d08
Thrynk <thomas.carin@isen.yncrea.fr>
2019-03-01 16:20:02 +0100

    splitChunks added

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    'production-dependencies': ['phaser']
+  },
+
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  }
+};
