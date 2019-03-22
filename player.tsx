68461c532182e47a05fa1086debd1f66d7d84bad
Lhotellier Théo <theo.lhotellier@isen.yncrea.fr>
2019-03-22 21:09:39 +0100

    adding player tilset

---
+++ player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="playertileset" tilewidth="16" tileheight="16" tilecount="684" columns="57">
+ <image source="assets/playertileset.png" width="918" height="203"/>
+</tileset>
