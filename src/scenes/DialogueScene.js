2bd5999735e337f2330a424c3d493fb4c4c63b21
Lhotellier Théo <theo.lhotellier@isen.yncrea.fr>
2019-05-05 15:19:18 +0200

    Dialogue Scene

---
+++ src/scenes/DialogueScene.js
@@ -0,0 +1,20 @@
+import Phaser from "phaser";
+import  { CST } from "../CST";
+
+export class DialogueScene extends Phaser.Scene {
+  constructor(){
+    super({
+        key: CST.SCENES.DIALOGUE
+    })
+    this.sentence="";
+    init(sentence){
+      this.sentence = sentence;
+    }
+
+    preload(){
+
+    }
+    create(){
+
+    }
+  }
