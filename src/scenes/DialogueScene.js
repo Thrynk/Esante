b12a136f304fa3316b8444415a378f07b310ee61
Lhotellier Théo <theo.lhotellier@isen.yncrea.fr>
2019-05-07 18:19:24 +0200

    Final Dialogue Scene and new Json with more options

--- src/scenes/DialogueScene.js
+++ src/scenes/DialogueScene.js
@@ -17,5 +17,13 @@ export class DialogueScene extends Phaser.Scene {
   }
   create(){
     console.log(this.sentence);
+    var chatboxCadran = this.add.image(400, 550, "chatboxCadran");
+    var sentence = this.add.text(20, 520, this.sentence, { fontSize: '16px', fill: '#000' });
+    sentence.setWordWrapWidth(760);
+    var scene = this.scene;
+    setTimeout(function(){scene.stop(CST.SCENES.DIALOGUE);},5000,this);
+    this.input.keyboard.on('keydown-' + 'F', function (event) {
+      this.scene.stop(CST.SCENES.DIALOGUE);
+    }, this);
   }
 }
